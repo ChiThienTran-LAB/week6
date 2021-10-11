27f7a166b996b1e8ff1849a753cbeebf4319957d
187IT09835 - TRAN CHI THIEN - T6 <thien.187it09835@vanlanguni.vn>
2021-10-11 20:59:33 +0700

    test code

---
+++ Tuan6/src/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner_a = new Scanner(System.in);
+        System.out.println("Nhập a: ");
+        int a = scanner_a.nextInt();
+        Scanner scanner_b = new Scanner(System.in);
+        System.out.println("Nhập b: ");
+        int b = scanner_b.nextInt();
+        if (a>b){
+           System.out.println("a>b");
+        }if (a==b) {
+            System.out.println("a=b");
+        } if(a<b) {
+          System.out.println("a<b");  
+        }
+    }
+}
